dda876512359dfc8475eab2550bac549c380b020
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-01 14:16:46 +0100

    Python Variables and Len Function

---
+++ Day1/pythonVariables.py
@@ -0,0 +1,17 @@
+name = input("What is your name?")
+
+print("Your name is " + name)
+
+name = "Robert"
+print(name)
+name = "Mirna"
+print(name) 
+
+''' 
+len function
+Gives the length of a string
+'''
+
+name = input("What is your name? ")
+length = len(name) 
+print("Your name has " + str(length) + " characters.")
