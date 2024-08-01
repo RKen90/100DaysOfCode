aece1cc3e6b6eb7cf65d6be343988f7fe5d10c9f
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-01 14:27:13 +0100

    Day 1 Challenge

---
+++ Day1/variablesChallenge.py
@@ -0,0 +1,17 @@
+# Switching the variables challenge
+
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+c = 0
+c = a
+a = b
+b = c
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
