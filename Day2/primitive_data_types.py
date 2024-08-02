85144605402acb53360c1a30f3658fb44ab1846f
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-02 09:07:13 +0100

    primitive data types

---
+++ Day2/primitive_data_types.py
@@ -0,0 +1,18 @@
+print(len("Hello"))
+print(len(str(12345)))
+
+'''
+Data Types
+
+Strings - A string/series of characters: "Hello"
+Subscripting - Pulling out a character from a string: "Hello"[0]
+
+Integers - Whole Numbers: 123456
+123_456_789
+
+Float - Floating point numbers: 3.14159
+
+Boolean - True of False
+'''
+
+print(123_456_789)
