caccb97c28055e5b3af9190f17bb6b8f0b4da438
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-02 09:43:23 +0100

    Number manipulation and F-Strings

---
+++ Day2/number_manipulation_f_strings.py
@@ -0,0 +1,14 @@
+# Round Function
+print(8/3)
+print(int(8/3))
+print(round(8/3))
+print(round(8/3, 2))
+
+# Floor Division
+print(8//3)
+
+# F-Strings
+score = 0
+height = 1.8
+isWinning = True
+print(f'Your score is {score}, you are {height}m tall, you are winning is {isWinning}')
