aacc00dc1165985d966ba490a86eb8fcd7186012
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-02 09:24:58 +0100

    Mathematical Operations

---
+++ Day2/mathematicalOperations.py
@@ -0,0 +1,32 @@
+'''
+Mathematical Operators
+
+'''
+
+# Addition
+print(4 + 3)
+
+# Subtraction
+print(4 - 3)
+
+# Multiplication
+print(4 * 3)
+
+# Division
+print(4/3)
+
+# Exponent: To the power of something
+# E.g 4 X 4 X 4
+print(4 ** 3)
+
+'''
+PEMDAS(LR) 
+()
+**
+* /
++ -
+Left 
+Right
+'''
+
+print(3 * 3 + 3 / 3 - 3)
