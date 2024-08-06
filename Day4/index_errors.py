ff5f66f976c7c1885ffad5b47c3c2ad47124a8dc
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-06 11:39:28 +0100

    Nested Lists

---
+++ Day4/index_errors.py
@@ -0,0 +1,12 @@
+# List of the Dirty Dozen
+dirty_dozen = ['Strawberries', 'Spinach', 'Kale', 'Nectarines', 'Apples', 'Grapes', 'Peaches', 'Cherries', 'Pears', 'Tomatoes', 'Celery', 'Potatoes']
+fruits = ['Strawberries', 'Nectarines', 'Apples', 'Grapes', 'Peaches', 'Cherries', 'Pears']
+vegetables = ['Spinach', 'Kale', 'Tomatoes', 'Celery', 'Potatoes']
+
+print(len(dirty_dozen))
+
+# Nested Lists
+dirty_dozen = [fruits, vegetables]
+print(len(dirty_dozen))
+print(dirty_dozen)
+print(dirty_dozen[0])
