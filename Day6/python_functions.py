bccc9598c96dfe68b5a7fe03a045ee103a10c171
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-07 09:18:34 +0100

    Functions

---
+++ Day6/python_functions.py
@@ -0,0 +1,24 @@
+'''
+Built in Functions
+- len()
+- int()
+- print()
+- range()
+
+Functions all work in essentially the same way
+- function
+- function + parenthesis: function()
+- performs a function/action
+'''
+
+print("Hello, I am the built in Print Function")
+
+# Making our own functions
+def my_function():
+    print("Hello")
+    print("I am a custom made function!")
+    print("Goodbye")
+    
+    
+# Calling the function
+my_function()
