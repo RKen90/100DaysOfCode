11b994f4d0024376b7d69e40718641c952570965
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-07 09:35:52 +0100

    While Loops

---
+++ Day6/while_loops.py
@@ -0,0 +1,13 @@
+fruits = ['Apples', 'Pears', 'Bananas', 'Dates', 'Orange']
+
+print(len(fruits))
+
+# For Loop
+for fruit in fruits:
+    print(f'Yum {fruit}')
+    
+fruit = 0
+# While Loop
+while fruit < len(fruits):
+    print(f'Test {fruits[fruit]}')
+    fruit += 1
