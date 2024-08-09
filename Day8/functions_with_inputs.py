62c7e18e174c39194c6161b401ecd5616626f916
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-09 09:38:19 +0100

    functions with inputs

---
+++ Day8/functions_with_inputs.py
@@ -0,0 +1,23 @@
+# Functions
+''' 
+Create a function called Greet
+Write 3 print statements inside the function
+call the function
+'''
+
+def greet():
+    print("This is my function")
+    print("It prints a line")
+    print("And another line")
+    
+greet()
+
+# Functions with Inputs
+def greetings(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+    print(f"Isn't the weather nice {name}")
+    
+greetings("Brobert")
+print("")
+greetings("Mirna")
