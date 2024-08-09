37b3e117e82328d7c4b5967ff2d33b595e293fcb
Robert Kennard <KennardR@cardiff.ac.uk>
2024-08-09 09:58:01 +0100

    Positional and Keyword Arguments

---
+++ Day8/positional_vs_keyword.py
@@ -0,0 +1,9 @@
+# Functions with more than 1 input
+# Two parameters (name, location)
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+    
+greet_with("Robert", "Pontypridd")
+greet_with(location="Bristol", name = "Brobert")
